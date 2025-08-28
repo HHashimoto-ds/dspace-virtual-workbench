a9ccfc2315f98cfabe35a708e079c2f519cc7814
HHashimoto <hhashimoto@dspace.jp>
2025-08-28 16:31:32 +0900

    modified:   PythonSDK_Turnlamp_AllPass/tests/conftest.py

--- PythonSDK_Turnlamp_AllPass/tests/conftest.py
+++ PythonSDK_Turnlamp_AllPass/tests/conftest.py
@@ -13,13 +13,23 @@ LOGGER = logging.getLogger(__name__)
 
 def handle_sigint(signum, frame):
     """Handle SIGINT (Ctrl+C) signal."""
-    LOGGER.error("SIGINT received. Cleaning up before exit...")
+    msg = "SIGINT received. Cleaning up before exit..."
+    # 複数の方法でメッセージを出力
+    print(msg, file=sys.stderr)
+    sys.stderr.write(msg + "\n")
+    sys.stderr.flush()
+    LOGGER.error(msg)
     # 必要なクリーンアップ処理をここに書く
     sys.exit(1)  # pytest を終了させる
     
 def handle_sigterm(signum, frame):
     """Handle SIGTERM signal."""
-    LOGGER.error("SIGTERM received. Cleaning up before exit...")
+    msg = "SIGTERM received. Cleaning up before exit..."
+    # 複数の方法でメッセージを出力
+    print(msg, file=sys.stderr)
+    sys.stderr.write(msg + "\n")
+    sys.stderr.flush()
+    LOGGER.error(msg)
     # 必要なクリーンアップ処理をここに書く
     sys.exit(1)  # pytest を終了させる
 
@@ -28,3 +38,5 @@ def pytest_sessionstart(session):
     """Register signal handlers at the start of pytest session."""
     signal.signal(signal.SIGINT, handle_sigint)
     signal.signal(signal.SIGTERM, handle_sigterm)
+    # キャプチャを無効化
+    session.config.option.capture = "no"
