0bf2197f034df25974ed26bd073eeece05f938ec
HHashimoto <hhashimoto@dspace.jp>
2025-08-28 17:42:24 +0900

    modified:   .github/workflows/one-time-simulation-and-test-with-ta-sdk-using-aws-linux-runner.yaml 	modified:   PythonSDK_Turnlamp_AllPass/tests/conftest.py

--- PythonSDK_Turnlamp_AllPass/tests/conftest.py
+++ PythonSDK_Turnlamp_AllPass/tests/conftest.py
@@ -1,5 +1,8 @@
-
+def pytest_sessionstart(session):
+    print('session started')
+    
 def pytest_sessionfinish(session, exitstatus):
+    print('session finished')
     if exitstatus == 2:  # 2 は KeyboardInterrupt の exit code
         print("KeyboardInterrupt により pytest が終了しました")
 
